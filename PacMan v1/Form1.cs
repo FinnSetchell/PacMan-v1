d328d0d03db305c5e0002cf63ce7908dc157d013
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2023-03-09 15:58:22 +0000

    added Ghost class with methods

--- PacMan v1/Form1.cs
+++ PacMan v1/Form1.cs
@@ -9,16 +9,21 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static PacMan_v1.Ghost;
 using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 namespace PacMan_v1
 {
     public partial class Form1 : Form
     {
-
         bool goUp, goDown, goLeft, goRight, isGameOver;
 
-        int score, playerSpeed, redGhostSpeed, yellowGhostSpeed, pinkGhostX, pinkGhostY;
+        int score, playerSpeed, ghostSpeed, yellowGhostSpeed, pinkGhostX, pinkGhostY;
+
+        private void redGhost_Click(object sender, EventArgs e)
+        {
+
+        }
 
         public Form1()
         {
@@ -29,10 +34,10 @@ namespace PacMan_v1
 
         private void keyisdown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.W) {goUp = true;}
-            if (e.KeyCode == Keys.S) {goDown = true;}
-            if (e.KeyCode == Keys.A) {goLeft = true;}
-            if (e.KeyCode == Keys.D) {goRight = true;}
+            if (e.KeyCode == Keys.W) { goUp = true; }
+            if (e.KeyCode == Keys.S) { goDown = true; }
+            if (e.KeyCode == Keys.A) { goLeft = true; }
+            if (e.KeyCode == Keys.D) { goRight = true; }
 
         }
 
@@ -59,31 +64,31 @@ namespace PacMan_v1
             {
                 pacman.Left -= playerSpeed;
                 pacman.Image = Properties.Resources.left;
-            } else if (goRight)
+            }
+            else if (goRight)
             {
                 pacman.Left += playerSpeed;
                 pacman.Image = Properties.Resources.right;
-            } else if (goUp)
+            }
+            else if (goUp)
             {
                 pacman.Top -= playerSpeed;
                 pacman.Image = Properties.Resources.Up;
-            } else if (goDown)
+            }
+            else if (goDown)
             {
                 pacman.Top += playerSpeed;
                 pacman.Image = Properties.Resources.down;
             }
-            
 
-
-            if(pacman.Left < -10)
+            if (pacman.Left < -10)
             {
                 pacman.Left = 680;
             }
-            if(pacman.Left  > 680)
+            if (pacman.Left > 680)
             {
                 pacman.Left = -10;
             }
-
             if (pacman.Top < -10)
             {
                 pacman.Top = 550;
@@ -97,6 +102,7 @@ namespace PacMan_v1
             {
                 if (x is PictureBox)
                 {
+                    // collects coin and adds to score
                     if ((string)x.Tag == "coin" && x.Visible)
                     {
                         if (pacman.Bounds.IntersectsWith(x.Bounds))
@@ -105,7 +111,7 @@ namespace PacMan_v1
                             x.Visible = false;
                         }
                     }
-                    
+                    // stops moving through walls
                     if ((string)x.Tag == "wall")
                     {
                         if (pacman.Bounds.IntersectsWith(x.Bounds))
@@ -114,12 +120,12 @@ namespace PacMan_v1
                             pacman.Top = prevTop;
                         }
 
-                        if (pinkGhost.Bounds.IntersectsWith(x.Bounds))
+                        if (Inky.Bounds.IntersectsWith(x.Bounds))
                         {
                             pinkGhostX = -pinkGhostX;
                         }
                     }
-
+                    // ends game if pacman touchy ghosts
                     if ((string)x.Tag == "ghost")
                     {
                         if (pacman.Bounds.IntersectsWith(x.Bounds))
@@ -130,76 +136,73 @@ namespace PacMan_v1
                     }
                 }
             }
-
-
-            // ghost movement
-            redGhost.Left += redGhostSpeed;
-            if (redGhost.Bounds.IntersectsWith(wall1.Bounds) || redGhost.Bounds.IntersectsWith(wall2.Bounds))
-            {
-                redGhostSpeed = -redGhostSpeed;
-            }
-
-            yellowGhost.Left += yellowGhostSpeed;
-            if (yellowGhost.Bounds.IntersectsWith(wall3.Bounds) || yellowGhost.Bounds.IntersectsWith(wall4.Bounds))
-            {
-                yellowGhostSpeed = -yellowGhostSpeed;
-            }
-
-            pinkGhost.Left -= pinkGhostX;
-            pinkGhost.Top -= pinkGhostY;
-            
-            if (pinkGhost.Top < 0 || pinkGhost.Top > 520)
-            {
-                pinkGhostY = -pinkGhostY;
-            }
-
-            if (pinkGhost.Right < 0 || pinkGhost.Right > 620)
-            {
-                pinkGhostX = -pinkGhostX;
-            }
-
-
-
+            // checks if game won
             if (score == 27)
             {
                 // game won
                 gameOver("You Win!");
             }
 
+
+
+            //**  ghost movement  **//
+            Blinky.Left += redGhostSpeed;
+            if (Blinky.Bounds.IntersectsWith(wall1.Bounds) || Blinky.Bounds.IntersectsWith(wall2.Bounds))
+            {
+                redGhostSpeed = -redGhostSpeed;
+            }
+
+            Pinky.Left += yellowGhostSpeed;
+            if (Pinky.Bounds.IntersectsWith(wall3.Bounds) || Pinky.Bounds.IntersectsWith(wall4.Bounds))
+            {
+                yellowGhostSpeed = -yellowGhostSpeed;
+            }
+
+            Inky.Left -= pinkGhostX;
+            Inky.Top -= pinkGhostY;
+
+            if (Inky.Top < 0 || Inky.Top > 520)
+            {
+                pinkGhostY = -pinkGhostY;
+            }
+
+            if (Inky.Right < 0 || Inky.Right > 620)
+            {
+                pinkGhostX = -pinkGhostX;
+            }
+            
         }
 
         private void resetGame()
         {
+            // Create an array of ghosts
+            List<Ghost> ghosts = new List<Ghost>();
+            ghosts.Add(new Ghost("Blinky", 289, 411, 1));
+            ghosts.Add(new Ghost("Pinky", 327, 411, 2));
+            ghosts.Add(new Ghost("Inky", 365, 411, 3));
+            //ghosts.Add(new Ghost("Clyde", 0, 0, 4));
+
+            // reset & hide labels
             txtGameOver.Visible = false;
             label3.Visible = false;
             txtScore.Text = "0";
             score = 0;
 
-            playerSpeed = 8;
-            redGhostSpeed = 5; 
-            yellowGhostSpeed = 5; 
-            pinkGhostX = 5; 
-            pinkGhostY = 5;
+            // resets positions
+            pacman.Left = 1;
+            pacman.Top = 402;
 
+            playerSpeed = 8;
             isGameOver = false;
 
-            pacman.Left = 29;
-            pacman.Top = 366;
-            redGhost.Left = 205;
-            redGhost.Top = 64;
-            yellowGhost.Left = 459;
-            yellowGhost.Top = 450;
-            pinkGhost.Left = 495;
-            pinkGhost.Top = 203;
-
-            foreach(Control x in this.Controls)
+            // makes all elements visible
+            foreach (Control x in this.Controls)
             {
                 if (x is PictureBox)
                 {
                     x.Visible = true;
                 }
             }
-
 
             gameTimer.Start();
         }
@@ -216,4 +219,70 @@ namespace PacMan_v1
         }
 
     }
+
+    public class Ghost
+    {
+        public string name;
+        public int x;
+        public int y;
+        public int speed;
+        public bool frightened;
+
+        public Ghost(string name, int x, int y, int speed)
+        {
+            this.name = name;
+            this.x = x;
+            this.y = y;
+            this.speed = speed;
+            this.frightened = false;
+        }
+
+        public enum GhostName
+        {
+            Blinky,
+            Pinky,
+            Inky,
+            Clyde
+        }
+        public enum GhostSpeed
+        {
+            Slow = 1,
+            Medium = 2,
+            Fast = 3,
+            VeryFast = 4
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+        public bool Frightened
+        {
+            get { return frightened; }
+            set { frightened = value; }
+        }
+        public void Move(int dx, int dy)
+        {
+            x += dx;
+            y += dy;
+        }
+    }
+
+    
 }
